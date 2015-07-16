800e76cb649cb72ac1c5820bf7d0f52cc74f8f12
Daniel Alves <da.ferreiraalves@gmail.com>
2015-07-16 19:27:17 -0300

    adicionando local

---
+++ config/autoload/local.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'db' => array(
+        'root' => array(
+            'driver'    => 'Pdo',
+            'username'  => 'root',
+            'password'  => '',
+            'dsn'       => 'mysql:dbname=avalizza;host=localhost',
+            'driver_options' => array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+            )
+        ),
+    ),
+
+    'service_manager' => array(
+        'aliases' => array(
+            'zend_db_adapter' => 'Zend\Db\Adapter\Adapter',
+        ),
+    ),
+
+    'zfctwig' => array(
+        'suffix' => 'html',
+    ),
+);
